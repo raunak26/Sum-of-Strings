dff0e864363000b07b3aa4066761430592c58fdd
Raunak Anand <46580556+raunak26@users.noreply.github.com>
2019-03-11 15:11:04 -0700

    Add files via upload

---
+++ stringsumnumbers.py
@@ -0,0 +1,18 @@
+user_input = input("Enter a string: ")
+number_sum= 0
+string = ""
+for character in user_input:
+    if character >= "0" and character <= "9":
+        string += character
+    elif character == ".":
+        string += character
+    elif string:
+        number_sum += float(string)
+        string = ""
+
+if string:
+    number_sum += float(string)
+
+print('%.2f' % number_sum)
+
+
